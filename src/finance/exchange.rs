04bcf344599af889c61dc1615ba200722102a546
veeso <christian.visintin1997@gmail.com>
2022-09-26 11:37:26 +0200

    exchange tests and conversions

--- src/finance/exchange.rs
+++ src/finance/exchange.rs
@@ -7,19 +7,10 @@ use chrono::Utc;
 use rust_decimal::{prelude::FromPrimitive, Decimal};
 use yahoo_finance::history;
 
+mod quote;
+use quote::{Quote, Quotes};
+
 const EUR_USD_SYMBOL: &str = "EURUSD=x";
-
-/// The collection of quotes ASC sorted by date
-pub struct Quotes {
-    quotes: Vec<Quote>,
-}
-
-/// A symbol quotation in on a specific date
-pub struct Quote {
-    pub date: DateTime<Utc>,
-    /// EUR price
-    pub price: Decimal,
-}
 
 pub struct Exchange {
     eur_usd: Quotes,
@@ -55,62 +46,14 @@ impl Exchange {
         Ok(data
             .into_iter()
             .filter(|x| x.timestamp >= from && x.timestamp <= to)
-            .map(|x| Quote {
-                date: x.timestamp,
-                price: Decimal::from_f64(x.close).unwrap_or_default(),
+            .map(|x| {
+                Quote::usd(
+                    x.timestamp,
+                    Decimal::from_f64(x.close).unwrap_or_default().round_dp(2),
+                )
             })
             .collect::<Vec<Quote>>()
             .into())
-    }
-}
-
-impl From<Vec<Quote>> for Quotes {
-    fn from(mut quotes: Vec<Quote>) -> Self {
-        // sort ASC by date
-        quotes.sort_by_key(|x| x.date);
-        Self { quotes }
-    }
-}
-
-impl Quotes {
-    /// Get valid price for date
-    pub fn price_at(&self, date: DateTime<Utc>) -> Decimal {
-        // get last
-        self.quotes
-            .iter()
-            .filter(|x| x.date <= date)
-            .last()
-            .map(|x| x.price)
-            .unwrap_or_default()
-    }
-
-    /// Convert quotes prices to EUR from USD
-    fn usd_to_eur(&mut self, conversion: &Quotes) -> anyhow::Result<()> {
-        debug!("converting quotes to EUR");
-        for quote in self.quotes.iter_mut() {
-            quote.usd_to_eur(conversion)?;
-        }
-        Ok(())
-    }
-}
-
-impl Quote {
-    /// Convert self price from USD to EUR
-    fn usd_to_eur(&mut self, conversion: &Quotes) -> anyhow::Result<()> {
-        let eur_change = match conversion.quotes.iter().find(|x| x.date == self.date) {
-            Some(quote) => quote.price,
-            None => anyhow::bail!(
-                "could not find any USD-EUR conversion value for {}",
-                self.date
-            ),
-        };
-        // convert => self.price : eur_change = x : 1.0
-        debug!(
-            "applying USD to EUR conversion; 1$ = {}€ at {}",
-            eur_change, self.date
-        );
-        self.price = self.price / eur_change;
-        Ok(())
     }
 }
 
@@ -126,7 +69,9 @@ mod test {
         let from = Utc.from_utc_datetime(&NaiveDate::from_ymd(2021, 1, 1).and_hms(0, 0, 0));
         let to = Utc.from_utc_datetime(&NaiveDate::from_ymd(2021, 12, 31).and_hms(23, 59, 59));
         let exchange = Exchange::new(from, to).unwrap();
-        assert_eq!(exchange.eur_usd.quotes.len(), 365);
+        let september23 =
+            Utc.from_utc_datetime(&NaiveDate::from_ymd(2021, 9, 23).and_hms(23, 59, 59));
+        assert_eq!(exchange.eur_usd.price_at(september23), dec!(1.17));
     }
 
     #[test]
@@ -136,5 +81,9 @@ mod test {
         let exchange = Exchange::new(from, to).unwrap();
         let quotes = exchange.get_symbol_quotes("AMZN").unwrap();
         // check price for 23/09/2022
+        let september23 =
+            Utc.from_utc_datetime(&NaiveDate::from_ymd(2022, 9, 23).and_hms(23, 59, 59));
+        // 113.78 $ =>
+        assert_eq!(quotes.price_at(september23), dec!(116.10));
     }
 }
---
+++ src/finance/exchange/quote.rs
@@ -0,0 +1,135 @@
+//! # Quote
+//!
+//! Quote type
+
+use chrono::prelude::*;
+use chrono::Utc;
+use rust_decimal::Decimal;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum Currency {
+    Usd,
+    Eur,
+}
+
+/// The collection of quotes ASC sorted by date
+pub struct Quotes {
+    quotes: Vec<Quote>,
+}
+
+/// A symbol quotation in on a specific date
+pub struct Quote {
+    pub date: DateTime<Utc>,
+    /// EUR price
+    pub price: Decimal,
+    /// Tracks current price currency
+    currency: Currency,
+}
+
+impl From<Vec<Quote>> for Quotes {
+    fn from(mut quotes: Vec<Quote>) -> Self {
+        // sort ASC by date
+        quotes.sort_by_key(|x| x.date);
+        Self { quotes }
+    }
+}
+
+impl Quotes {
+    /// Get valid price for date
+    pub fn price_at(&self, date: DateTime<Utc>) -> Decimal {
+        // get last
+        self.quotes
+            .iter()
+            .filter(|x| x.date <= date)
+            .last()
+            .map(|x| x.price)
+            .unwrap_or_default()
+    }
+
+    /// Convert quotes prices to EUR from USD
+    pub fn usd_to_eur(&mut self, conversion: &Quotes) -> anyhow::Result<()> {
+        debug!("converting quotes to EUR");
+        for quote in self.quotes.iter_mut() {
+            quote.usd_to_eur(conversion)?;
+        }
+        Ok(())
+    }
+}
+
+impl Quote {
+    /// Create a new Quote with USD price
+    pub fn usd(date: DateTime<Utc>, price: Decimal) -> Self {
+        Self {
+            date,
+            price,
+            currency: Currency::Usd,
+        }
+    }
+
+    /// Convert self price from USD to EUR
+    fn usd_to_eur(&mut self, conversion: &Quotes) -> anyhow::Result<()> {
+        if self.currency != Currency::Usd {
+            anyhow::bail!("current currency is not USD, but {:?}", self.currency);
+        }
+        let eur_change = conversion.price_at(self.date);
+        // convert => self.price : eur_change = x : 1.0
+        debug!(
+            "applying USD to EUR conversion; 1$ = {}€ at {}",
+            eur_change, self.date
+        );
+        self.price = (self.price / eur_change).round_dp(2);
+        // set currency to eur
+        self.currency = Currency::Eur;
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod test {
+
+    use super::*;
+
+    use pretty_assertions::assert_eq;
+
+    #[test]
+    fn should_make_quote() {
+        let date = Utc.from_utc_datetime(&NaiveDate::from_ymd(2022, 9, 23).and_hms(12, 0, 0));
+        let quote = Quote::usd(date, dec!(120.32));
+        assert_eq!(quote.date, date);
+        assert_eq!(quote.price, dec!(120.32));
+        assert_eq!(quote.currency, Currency::Usd);
+    }
+
+    #[test]
+    fn should_convert_price_to_eur() {
+        let date = Utc.from_utc_datetime(&NaiveDate::from_ymd(2022, 9, 23).and_hms(12, 0, 0));
+        let mut quote = Quote::usd(date, dec!(120.32));
+        assert!(quote.usd_to_eur(&usd_to_eur_table()).is_ok());
+        assert_eq!(quote.price, dec!(122.78));
+        assert_eq!(quote.currency, Currency::Eur);
+    }
+
+    #[test]
+    fn should_not_convert_price_to_eur_twice() {
+        let date = Utc.from_utc_datetime(&NaiveDate::from_ymd(2022, 9, 23).and_hms(12, 0, 0));
+        let mut quote = Quote::usd(date, dec!(120.32));
+        assert!(quote.usd_to_eur(&usd_to_eur_table()).is_ok());
+        // error on second conversion
+        assert!(quote.usd_to_eur(&usd_to_eur_table()).is_err());
+        assert_eq!(quote.price, dec!(122.78));
+        assert_eq!(quote.currency, Currency::Eur);
+    }
+
+    fn usd_to_eur_table() -> Quotes {
+        Quotes::from(vec![
+            Quote::usd(quote_date(2022, 1, 1), dec!(1.14)),
+            Quote::usd(quote_date(2022, 9, 1), dec!(1.00)),
+            Quote::usd(quote_date(2022, 9, 22), dec!(0.98)),
+            Quote::usd(quote_date(2022, 9, 26), dec!(0.97)),
+        ])
+    }
+
+    fn quote_date(year: i32, month: u32, day: u32) -> DateTime<Utc> {
+        Utc.from_utc_datetime(&NaiveDate::from_ymd(year, month, day).and_hms(12, 0, 0))
+    }
+}
